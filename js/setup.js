50bf019b4223dfa0f9fe0f6067afb375b2e9b524
chadsito <wunschecarlos@gmail.com>
2022-02-21 21:53:18 -0500

    shot 1 twin

--- js/setup.js
+++ js/setup.js
@@ -6,6 +6,8 @@
 // Una vez que se cargaron todos los .js, recien ahi declarar objetos y arrancar engine
 window.onload = function(){
 
+  console.clear();
+
   // Game screen initial dimensions
   const initW = 160;
   const initH = 120;
@@ -23,7 +25,7 @@ window.onload = function(){
   // Create graphics object to store all images
   const gfx     = new Gfx();
   // Create Engine passing: minimum step in ms, FPS, updateFunction, renderFunction
-  const engine  = new Engine(1000/60, 165, update, render);
+  const engine  = new Engine(1000/60, 60, update, render);
   // Create Input object
   const input   = new Input();
   // Create Game passing: initW, initH, rawInput Map, and graphics object
@@ -37,22 +39,15 @@ window.onload = function(){
 
 
 
-////////////////////////////////
+///////////////////////////////
 // GENERAL PURPOSE FUNCTIONS //
-////////////////////////////////
-
-// function setSprite(obj, png, scale = false, scaleValue = 32){
-//   obj.sprite = document.createElement('img'); // same as new Image()
-//   // Source PNG always needs to be scaled by x32 + optimized using online PNG Optimizer!
-//   obj.sprite.src = png;
-//   if (scale) {
-//       //Scale it down to initial size (Only when it finishes loading png)
-//       obj.sprite.addEventListener('load', function() {
-//           obj.sprite.width  = obj.sprite.width / scaleValue; 
-//           obj.sprite.height = obj.sprite.height / scaleValue;
-//           removeEventListener('load', obj.sprite);
-//       });
-//   }
-// };
+///////////////////////////////
 
 function toRadians(degrees){return degrees * Math.PI/180};
+
+
+////////////
+// TO-DOs //
+////////////
+
+// Change sprite property in various objects game.js and then the references in display.js
